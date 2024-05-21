b51dbb6e21f5be0da31332d8f96de97d6fba794f
Shenton <20748461@student.curtin.edu.au>
2024-05-21 21:47:31 +0800

    created main file in c file

---
+++ 8080Emulator/Dissasembler.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main(int argc, int** argv)
+{
+	FILE *file = fopen(argv[1] "rb");
+	if (file == NULL)
+	{
+		printf("error: Couldn't open %s\n", argv[1]);    
+          	exit(1);
+	}
+
+	fseek(f, 0L, SEEK_END);
+	int fsize = ftell(file);
+	fseek(f, 0L, SEEK_SET);
+
+	unsigned char *buffer = malloc(fsize);
+
+	fread(buffer, fsize, 1, f);
+	fclose(file);
+
+	int pc = 0; // program counter
+	
+	while (pc < fsize)
+	{
+		pc =+ 1;
+	}
+	
+	return 0;
+}
+
+int Dissasembler8080p(unsinged char *buffer, int pc)
+{
+	unsigned char* codeLine = &buffer[pc];
+	int opbytes = 0; // size in bytes of operation
+	printf("%04x", pc); // printing out byte # in buffer to 4 digit number
+			    
+	switch (*codeLine) 
+	{
+		case 0x00: break;
+	}
+
+	printf("\n");
+
+	return opbytes;
+}
